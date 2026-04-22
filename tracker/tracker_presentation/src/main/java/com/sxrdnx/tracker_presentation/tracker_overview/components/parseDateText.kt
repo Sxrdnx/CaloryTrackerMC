692942979b940cd21d3c12374a8169320d56f090
Sxrdnx <serdna.alejandro@gmail.com>
2026-04-22 16:55:29 -0600

    FEAT: create day selector and format date for ui

---
+++ tracker/tracker_presentation/src/main/java/com/sxrdnx/tracker_presentation/tracker_overview/components/parseDateText.kt
@@ -0,0 +1,27 @@
+package com.sxrdnx.tracker_presentation.tracker_overview.components
+
+import com.sxrdnx.core.R
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+
+@Composable
+fun ParseDateText(date: LocalDate):String {
+    val today = LocalDate.now()
+
+    return when(date){
+        today->{
+            stringResource(id = R.string.today)
+        }
+        today.plusDays(1)->{
+            stringResource(id = R.string.yesterday)
+        }
+        today.minusDays(1)->{
+            stringResource(id = R.string.tomorrow)
+        }
+        else -> {
+            DateTimeFormatter.ofPattern("dd LLLL").format(date)
+        }
+    }
+}
